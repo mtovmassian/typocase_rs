5e977e55bdee5c32efe78237273fda0d7d426757
Martin Tovmassian <martin.tovmassian@protonmail.com>
2020-01-21 14:27:20 +0100

    feat: impl split on uppercase letter func

--- src/str_comps.rs
+++ src/str_comps.rs
@@ -10,25 +10,43 @@ impl StringCompounds {
         return StringCompounds { _string: _string.clone() };
     }
 
-    pub fn split_on_special_char(&self) -> Vec<String> {
-        let alphanum_chars = self._string.chars()
+    pub fn split_on_uppercase_letters(&self) -> Vec<String> {
+        let alphanums: String = self._string.chars()
+        .filter(|_char|_char.is_alphanumeric())
         .map(|_char| {
-            if _char.is_alphanumeric() {
-                return _char;
+            if _char.is_lowercase() {
+                return _char.to_string();
             } else {
-                return WHITE_SPACE;
+                return format!("{}{}", WHITE_SPACE, _char.to_lowercase());
             }
         })
-        .collect::<Vec<char>>();
+        .collect::<String>();
 
-        let compounds = alphanum_chars.iter().collect::<String>()
-        .split_whitespace()
-        .map(|s| String::from(s).to_lowercase())
-        .collect::<Vec<String>>();
-        println!("{:?}", compounds);
-        println!("{}", self._string);
+        let compounds = self.split_on_whitespace(alphanums);
 
         return compounds;
+    }
+
+    pub fn split_on_special_char(&self) -> Vec<String> {
+        let alphanums: String = self._string.chars()
+        .map(|_char| {
+            if _char.is_alphanumeric() {
+                return _char.to_string().to_lowercase();
+            } else {
+                return WHITE_SPACE.to_string();
+            }
+        })
+        .collect::<String>();
+
+        let compounds = self.split_on_whitespace(alphanums);
+
+        return compounds;
+    }
+
+    fn split_on_whitespace(&self, _string: String) -> Vec<String> {
+        return _string.split_whitespace()
+        .map(|s| String::from(s))
+        .collect::<Vec<String>>();
     }
 
 }
--- tests/test_str_comps.rs
+++ tests/test_str_comps.rs
@@ -1,6 +1,78 @@
 extern crate typocase_rs;
 
 #[cfg(test)]
+mod test_split_on_upper_case_letters {
+    use typocase_rs::str_comps;
+
+    fn fixture(_string: &String) -> Vec<String> {
+        let instance = str_comps::StringCompounds::from(_string);
+
+        return instance.split_on_uppercase_letters();
+    }
+
+    #[test]
+    fn case_empty_string() {
+        assert_eq!(
+            fixture(&String::from("")),
+            Vec::new() as Vec<String>
+        );
+    }
+
+    #[test]
+    fn case_uppercase_letter_sep() {
+        assert_eq!(
+            fixture(&String::from("AbcDefGhi")),
+            vec!["abc", "def", "ghi"]
+        );
+        assert_eq!(
+            fixture(&String::from("abcDefGhi")),
+            vec!["abc", "def", "ghi"]
+        );
+    }
+
+    #[test]
+    fn case_uppercase_letter_sep_and_leading_trailing_special_char() {
+        assert_eq!(
+            fixture(&String::from("_AbcDefGhi_")),
+            vec!["abc", "def", "ghi"]
+        );
+    }
+
+    #[test]
+    fn case_only_uppercase_letter_sep() {
+        assert_eq!(
+            fixture(&String::from("ABCDEFGHI")),
+            vec!["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        );
+    }
+
+    #[test]
+    fn case_uppercase_letter_sep_and_special_char_sep() {
+        assert_eq!(
+            fixture(&String::from("A_b_c_D_e_f_G_h_i")),
+            vec!["abc", "def", "ghi"]
+        );
+    }
+
+    #[test]
+    fn case_no_uppercase_letter_sep() {
+        assert_eq!(
+            fixture(&String::from("abcdefghi")),
+            vec!["abcdefghi"]
+        );
+    }
+
+    #[test]
+    fn case_no_uppercase_letter_sep_and_special_char_sep() {
+        assert_eq!(
+            fixture(&String::from("abc_def_ghi")),
+            vec!["abcdefghi"]
+        );
+    }
+
+}
+
+#[cfg(test)]
 mod test_split_on_special_char {
     use typocase_rs::str_comps;
 
@@ -51,7 +123,7 @@ mod test_split_on_special_char {
     }
 
     #[test]
-    fn case_special_char_sep_and_upper_case_letters() {
+    fn case_special_char_sep_and_uppercase_letters() {
         assert_eq!(
             fixture(&String::from("ABC_DEF_GHI")),
             vec!["abc", "def", "ghi"]
